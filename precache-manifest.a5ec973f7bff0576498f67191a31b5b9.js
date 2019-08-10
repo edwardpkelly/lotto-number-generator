6b367afb514d8c672264b52c2606cc42ed525b76
Edward P. Kelly <ed@edkelly.net>
2019-08-10 19:14:03 -0400

    Updates

---
+++ precache-manifest.a5ec973f7bff0576498f67191a31b5b9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3835f3f8395753399243d36a20bcff5",
+    "url": "/lotto-number-generator/index.html"
+  },
+  {
+    "revision": "90ba0109b70409ab1918",
+    "url": "/lotto-number-generator/static/css/main.5bc1df2a.chunk.css"
+  },
+  {
+    "revision": "123fb41c3fb46a6d1e24",
+    "url": "/lotto-number-generator/static/js/2.a5adb38c.chunk.js"
+  },
+  {
+    "revision": "90ba0109b70409ab1918",
+    "url": "/lotto-number-generator/static/js/main.322d131b.chunk.js"
+  },
+  {
+    "revision": "ed185442e571234120ef",
+    "url": "/lotto-number-generator/static/js/runtime~main.75585b54.js"
+  }
+]);
